e1e5796b9039899e5c44ce2e20398da3a45dd19d
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-01-05 17:31:16 -0600

    styling and layouts for index page

--- models/productSeed.js
+++ models/productSeed.js
@@ -17,6 +17,25 @@ module.exports= [{
         description: 'A tennis ball',
         price: 5,
         qty: 50
+    },{
+        name: 'hockey sticks',
+        img: 'https://images.unsplash.com/photo-1580670759122-71ec87c409ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        description: 'Premium hockey sticks',
+        price: 40,
+        qty: 5
+    }, {
+        name: 'baseball gloves',
+        img: 'https://images.unsplash.com/photo-1620740528428-59fb523a484a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        description: 'Rawlings baseball gloves',
+        price: 150,
+        qty: 40
+    }, {
+        name: 'Skateboards',
+        img: 'https://images.unsplash.com/photo-1520045892732-304bc3ac5d8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        description: 'Element skateboards',
+        price: 200,
+        qty: 1
+
     }
 
 
