1db79be9a4be8fbc152e0d603308863f835be2a0
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-01-05 20:11:58 -0600

    styling for all pages finished

--- models/productSeed.js
+++ models/productSeed.js
@@ -1,30 +1,30 @@
 module.exports= [{
-        name: 'Basketball',
+        name: 'Basketballs',
         img: 'https://images.unsplash.com/photo-1519861531473-9200262188bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
         description: "A basketball",
         price: 10,
         qty: 20
     }, {
-        name: 'baseball',
+        name: 'Baseballs',
         img: 'https://images.unsplash.com/photo-1521941651707-748bdbae77e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
         description: "A baseball",
         price: 10,
         qty: 30
         
     }, {
-        name: 'tennis ball',
+        name: 'Tennis balls',
         img: 'https://images.unsplash.com/photo-1519611103964-90f61a50d3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=868&q=80',
         description: 'A tennis ball',
         price: 5,
         qty: 50
     },{
-        name: 'hockey sticks',
+        name: 'Hockey sticks',
         img: 'https://images.unsplash.com/photo-1580670759122-71ec87c409ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         description: 'Premium hockey sticks',
         price: 40,
         qty: 5
     }, {
-        name: 'baseball gloves',
+        name: 'Baseball gloves',
         img: 'https://images.unsplash.com/photo-1620740528428-59fb523a484a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
         description: 'Rawlings baseball gloves',
         price: 150,
